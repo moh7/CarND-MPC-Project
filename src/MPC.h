651febbfc5ee5230d81c8f435275cb30c5407bfe
unknown <mohsen.samadani@gmail.com>
2018-01-10 10:46:53 -0500

    First commit.

---
+++ src/MPC.h
@@ -0,0 +1,39 @@
+#ifndef MPC_H
+#define MPC_H
+
+#include <vector>
+#include "Eigen-3.3/Eigen/Core"
+
+#define NUMBER_OF_STEPS 20
+#define DT = 0.05
+
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
+// simulator around in a circle with a constant steering angle and velocity on a
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
+// This is the length from front to CoG that has a similar radius.
+#define LF 2.67
+
+// reference speed
+#define V_REF 50
+
+using namespace std;
+
+class MPC {
+
+ public:
+  MPC();
+
+  virtual ~MPC();
+
+  // Solve the model given an initial state and polynomial coefficients.
+  // Return the first actuations.
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+};
+
+#endif /* MPC_H */
