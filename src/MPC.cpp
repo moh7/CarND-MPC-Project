92ed0c2955f4aa7ac5d4c262647450d4df750274
Mohsen Samadani <mohsen.samadani@gmail.com>
2018-01-24 17:02:25 -0500

    First commit.

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -221,6 +221,7 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   // {...} is shorthand for creating a vector, so auto x1 = {1.0,2.0}
   // creates a 2 element double vector.
   std::vector<int> actuation_values(2);
-  actuation_values << solution.x[delta_start],  solution.x[a_start];
+  actuation_values[0] = solution.x[delta_start];
+  actuation_values[1] = solution.x[a_start];
   return actuation_values;
 }
